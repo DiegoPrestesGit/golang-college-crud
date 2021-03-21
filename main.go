25bb1c19346413a7a37e730efde09ae8ddf9335f
Diego Prestes <diegoprestes.git@hotmail.com>
2021-03-21 20:27:17 -0300

    feat: add route

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ func handleRequests() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", helloWorld).Methods("GET")
 	router.HandleFunc("/api/golang/users", ALlUsers).Methods("GET")
+	router.HandleFunc("/api/golang/users/{id}", FindUserById).Methods("GET")
 	router.HandleFunc("/api/golang/users", NewUser).Methods("POST")
 	log.Fatal(http.ListenAndServe(":5666", router))
 }
