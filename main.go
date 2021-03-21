80971304234039ce13962e02917905b87d3bb9c5
Diego Prestes <diegoprestes.git@hotmail.com>
2021-03-21 18:45:31 -0300

    feat: new routes working in endpoint

--- main.go
+++ main.go
@@ -15,7 +15,9 @@ func helloWorld(w http.ResponseWriter, r *http.Request) {
 func handleRequests() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", helloWorld).Methods("GET")
-	log.Fatal(http.ListenAndServe(":1111", router))
+	router.HandleFunc("/api/golang/users", ALlUsers).Methods("GET")
+	router.HandleFunc("/api/golang/users", NewUser).Methods("POST")
+	log.Fatal(http.ListenAndServe(":5666", router))
 }
 
 func main() {
