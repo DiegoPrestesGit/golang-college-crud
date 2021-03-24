ac1d830b42bcefb01d52df6dc14acf20a9b3ed2b
Diego Prestes (DPS) <diegoprestes.git@hotmail.com>
2021-03-23 23:46:49 -0300

    feat: add database configuration

--- go.mod
+++ go.mod
@@ -4,7 +4,8 @@ go 1.15
 
 require (
 	github.com/gorilla/mux v1.8.0
-	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/jinzhu/gorm v1.9.16
+	gorm.io/driver/postgres v1.0.8
 	gorm.io/gorm v1.21.4
 
 )
--- user.go
+++ user.go
@@ -3,7 +3,37 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/jinzhu/gorm"
+	"github.com/jinzhu/gorm/dialects/postgres"
 )
+
+// dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
+// db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+var db *gorm.DB
+var err error
+
+type User struct {
+	gorm.Model
+	id         string
+	name       string
+	email      string
+	password   string
+	created_at string
+	updated_at string
+}
+
+func InitialMigration() {
+	database_config := "host:localhost user=postgres password=login-password dbname=postgres port=5433 sslmode=disable"
+	db, err := gorm.Open(postgres., &gorm.Config{})
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("Failed to connect to database")
+	}
+	defer db.Close()
+	db.AutoMigrate((&User{}))
+}
 
 func ALlUsers(response http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(response, "allUsers endpoint hit")
