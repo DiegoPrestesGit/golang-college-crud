41658e513bec562dd1664858abcb0bf4579f528b
Diego Prestes <diegoprestes.git@hotmail.com>
2021-03-24 22:22:43 -0300

    feat: connecting to database, needs some refactor

--- user.go
+++ user.go
@@ -5,34 +5,38 @@ import (
 	"net/http"
 
 	"github.com/jinzhu/gorm"
-	"github.com/jinzhu/gorm/dialects/postgres"
 )
-
-// dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
-// db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 var db *gorm.DB
 var err error
 
 type User struct {
 	gorm.Model
-	id         string
-	name       string
-	email      string
-	password   string
-	created_at string
-	updated_at string
+	Id         string
+	Name       string
+	Email      string
+	Password   string
+	Created_at string
+	Updated_at string
 }
 
 func InitialMigration() {
-	database_config := "host:localhost user=postgres password=login-password dbname=postgres port=5433 sslmode=disable"
-	db, err := gorm.Open(postgres., &gorm.Config{})
+	host := "localhost"
+	user := "postgres"
+	dbname := "postgres"
+	password := "login-password"
+	port := "5433"
+
+	database_config := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s port=%s", host, user, dbname, password, port)
+	db, err := gorm.Open("postgres", database_config)
 	if err != nil {
 		fmt.Println(err.Error())
 		panic("Failed to connect to database")
+	} else {
+		fmt.Println("connected")
 	}
 	defer db.Close()
-	db.AutoMigrate((&User{}))
+	db.AutoMigrate(&User{})
 }
 
 func ALlUsers(response http.ResponseWriter, request *http.Request) {
