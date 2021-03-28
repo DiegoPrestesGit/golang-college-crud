7aef319e31f655c82f21c1980a066c142a515642
Diego Prestes (DPS) <diegoprestes.git@hotmail.com>
2021-03-28 17:40:03 -0300

    style: changing code position

--- main.go
+++ main.go
@@ -11,8 +11,8 @@ import (
 func handleRequests() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/api/golang/users", ALlUsers).Methods("GET")
-	router.HandleFunc("/api/golang/users/{id}", FindUserById).Methods("GET")
 	router.HandleFunc("/api/golang/users", NewUser).Methods("POST")
+	router.HandleFunc("/api/golang/users/{id}", FindUserById).Methods("GET")
 	log.Fatal(http.ListenAndServe(":5666", router))
 }
 
